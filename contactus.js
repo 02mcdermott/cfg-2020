5f77f60c75b5ba8a4b75ff2d3bf273ce2709d7a0
mcderms <60071706+02mcdermott@users.noreply.github.com>
2020-03-09 21:28:18 +0000

    change map

--- contactus.js
+++
@@ -1,7 +0,0 @@
-function myMap() {
-    var mapProp= {
-      center:new google.maps.LatLng(51.5074,0.1278),
-      zoom:5,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    }
